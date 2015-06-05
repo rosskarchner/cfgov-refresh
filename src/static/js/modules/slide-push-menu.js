8a8c2dc4e990e84f86a99461310efdf95a05a49a
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2015-06-05 15:06:13 -0400

    code review

--- src/static/js/modules/slide-push-menu.js
+++ src/static/js/modules/slide-push-menu.js
@@ -4,66 +4,80 @@
 
 'use strict';
 
-require( './jquery/cfpb-aria-button' ).init();
 var $ = require( 'jquery' );
+require( './jquery/cfpb-aria-button' ).init();
+
+var _$window = $( window );
+var _$body = $( 'body' );
+var _$slidingNav = $( '.sliding-nav' );
+var _$slidingNavTrigger = $( '.sliding-nav_trigger' );
+var _$slidingNavNav = $( '.sliding-nav_nav' );
+var _$slidingNavPage = $( '.sliding-nav_page' );
+var _$slidingNavPageOverlay = $( '.sliding-nav_page-overlay' );
 
 function init() {
-  var $body = $( 'body' ),
-      $slidingNav = $( '.sliding-nav' ),
-      $slidingNavTrigger = $( '.sliding-nav_trigger' ),
-      $slidingNavNav = $( '.sliding-nav_nav' ),
-      $slidingNavPage = $( '.sliding-nav_page' ),
-      $slidingNavPageOverlay = $( '.sliding-nav_page-overlay' );
-
-  $slidingNavTrigger.click( function( e ) {
-    e.preventDefault();
-
-    // First deal with the filters button if it exists.
-    if ( $( '.l-sidenav' ).hasClass( 'is-open' ) ) {
-      $( '.l-sidenav-btn' ).trigger( 'click' );
-    }
-
-    if ( $slidingNav.hasClass( 'is-open' ) ) {
-      window.setTimeout( function() {
-        $slidingNavPage.removeClass( 'is-scroll-disabled' );
-      }, 200 );
-      $slidingNav.removeClass( 'is-open' );
-      $slidingNavPageOverlay.off( 'click' );
-    } else {
-      $slidingNav.addClass( 'is-open' );
-      $( window ).scroll( slidingNavStopScroll );
-      $slidingNavPageOverlay.click( function( evt ) {
-        evt.preventDefault();
-        $( $slidingNavTrigger[0] ).trigger( 'click' );
-      } );
-    }
-    $body.scrollTop( 0 );
-  } );
-
-  function slidingNavStopScroll() {
-    if ( parseInt( $slidingNavPage.css( 'margin-right' ), 10 ) < 0 ) {
-      $slidingNavPage.addClass( 'is-scroll-disabled' );
-      $slidingNavNav.css( 'min-height', $( window ).height() );
-      $( window ).off( 'scroll', slidingNavStopScroll );
-    }
-  }
+  _$slidingNavTrigger.click( _slidingNavTriggerClick );
 
   // Expanding list
   // TODO: Determine if we should actually use the cfpbAriaButton plugin.
   $( '.list-expanding_trigger' ).cfpbAriaButton();
-  $( '.list-expanding_trigger' ).click( function( e ) {
-    e.preventDefault();
-    $( this ).next().find( '.list-expanding_child-list' ).slideToggle( 100 );
-  } );
-  $( '.list-expanding_trigger' ).keyup( function( e ) {
-    // Space key pressed.
-    if ( e.which === 32 ) {
-      e.preventDefault();
-      $( this ).next().find( '.list-expanding_child-list' ).slideToggle( 100 );
-    }
-  } );
+  $( '.list-expanding_trigger' ).click( _listExpandingTriggerClick );
+  $( '.list-expanding_trigger' ).keyup( _listExpandingTriggerKeyUp );
+
   // Hide the child lists initially.
   $( '.list-expanding_child-list' ).hide();
+}
+
+function _listExpandingTriggerClick( e ) {
+  e.preventDefault();
+  $( this ).next().find( '.list-expanding_child-list' ).slideToggle( 100 );
+}
+
+function _listExpandingTriggerKeyUp( e ) {
+  // Return if the space key was not pressed.
+  if ( e.which !== 32 ) {
+    return;
+  }
+  e.preventDefault();
+  $( this ).next().find( '.list-expanding_child-list' ).slideToggle( 100 );
+}
+
+function _slidingNavTriggerClick( e ) {
+  e.preventDefault();
+
+  // First deal with the filters button if it exists.
+  if ( $( '.l-sidenav' ).hasClass( 'is-open' ) ) {
+    $( '.l-sidenav-btn' ).trigger( 'click' );
+  }
+
+  if ( _$slidingNav.hasClass( 'is-open' ) ) {
+    //window.setTimeout( _slidingNavDisabledDelay, 200 );
+    _$slidingNav.removeClass( 'is-open' );
+    _$slidingNavPageOverlay.off( 'click' );
+  } else {
+    _$slidingNav.addClass( 'is-open' );
+    _$window.scroll( _slidingNavStopScroll );
+    _$slidingNavPageOverlay.click( _slidingNavPageOverlayClick );
+  }
+  _$body.scrollTop( 0 );
+}
+
+function _slidingNavDisabledDelay() {
+  _$slidingNavPage.removeClass( 'is-scroll-disabled' );
+}
+
+function _slidingNavPageOverlayClick( e ) {
+  e.preventDefault();
+  $( _$slidingNavTrigger[0] ).trigger( 'click' );
+}
+
+function _slidingNavStopScroll() {
+  if ( parseInt( _$slidingNavPage.css( 'margin-right' ), 10 ) >= 0 ) {
+    return;
+  }
+  _$slidingNavPage.addClass( 'is-scroll-disabled' );
+  _$slidingNavNav.css( 'min-height', _$window.height() );
+  _$window.off( 'scroll', _slidingNavStopScroll );
 }
 
 // Expose public methods.
