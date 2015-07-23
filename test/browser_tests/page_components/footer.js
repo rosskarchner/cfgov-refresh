77de95b548f04ff14d87c80866b7e80d22a691ae
Steven Bellamy <steven.bellamy@cfpb.gov>
2015-07-23 08:40:00 -0400

    Code Cleanup

--- test/browser_tests/page_components/footer.js
+++ test/browser_tests/page_components/footer.js
@@ -7,7 +7,6 @@ function Footer() {
 
   this.footer = element( by.css( '.footer' ) );
   this.navList = this.footer.element( by.css( '.footer_nav-list' ) );
-
   this.navLinks = element.all( by.css( '.footer-middle-left .footer_list a, .footer-middle-right .footer_list a' ) );
   this.post = this.footer.element( by.css( '.footer-post' ) );
   this.shareList = this.footer.element( by.css( '.footer_share-icon-list' ) );
