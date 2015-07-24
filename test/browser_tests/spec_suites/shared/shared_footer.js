d59e16dd6fa8b0ad43e83530dea0a38410d2f97b
Steven Bellamy <steven.bellamy@cfpb.gov>
2015-07-24 08:38:44 -0400

    Revisions for Code Review

--- test/browser_tests/spec_suites/shared/shared_footer.js
+++ test/browser_tests/spec_suites/shared/shared_footer.js
@@ -1,40 +1,40 @@
 'use strict';
 
-var Footer = require( '../../page_components/footer.js' );
+var Footer = require( '../../shared_components/footer.js' );
 
 describe( 'The Footer Component', function() {
-  var _component;
+  var _sharedComponent;
   var _linkLookupArray = [ 'accessibility',
-                          'foia',
-                          'office of inspector general',
-                          'open government',
-                          'plain writing',
-                          'privacy, policy & legal notices',
-                          'tbd content',
-                          'usa.gov'
+                           'foia',
+                           'office of inspector general',
+                           'open government',
+                           'plain writing',
+                           'privacy, policy & legal notices',
+                           'tbd content',
+                           'usa.gov'
                         ];
 
   beforeEach( function() {
-    _component = new Footer();
-    _component.get();
+    _sharedComponent = new Footer();
+    _sharedComponent.get();
   } );
 
   it( 'should properly load in a browser', function() {
-      expect( _component.footer.isPresent() ).toBe( true );
+      expect( _sharedComponent.footer.isPresent() ).toBe( true );
     }
   );
 
   it( 'should include navList', function() {
-      expect( _component.navList.isPresent() ).toBe( true );
+      expect( _sharedComponent.navList.isPresent() ).toBe( true );
     }
   );
 
   it( 'should include navLinks and navLinks should be valid', function() {
-    _component.navLinks.count().then( function( count ) {
+    _sharedComponent.navLinks.count().then( function( count ) {
       expect( count > 0 ).toBe( true );
     } );
 
-    _component.navLinks.each( function( element ) {
+    _sharedComponent.navLinks.each( function( element ) {
 
       /* eslint-disable max-nested-callbacks */
       element.getText().then( function( text ) {
@@ -48,17 +48,17 @@ describe( 'The Footer Component', function() {
   } );
 
   it( 'should include post', function() {
-      expect( _component.post.isPresent() ).toBe( true );
+      expect( _sharedComponent.post.isPresent() ).toBe( true );
     }
   );
 
   it( 'should include shareList', function() {
-      expect( _component.shareList.isPresent() ).toBe( true );
+      expect( _sharedComponent.shareList.isPresent() ).toBe( true );
     }
   );
 
   it( 'should include officialWebsite', function() {
-      expect( _component.officialWebsite.isPresent() ).toBe( true );
+      expect( _sharedComponent.officialWebsite.isPresent() ).toBe( true );
     }
   );
 
