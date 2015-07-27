9574c57bd3d3c1137dd7185fb86d00396a1b0783
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2015-07-27 10:52:25 -0400

    code review edit

--- test/browser_tests/page_objects/page_office.js
+++ test/browser_tests/page_objects/page_office.js
@@ -11,56 +11,33 @@ function OfficePage() {
 
   this.pageTitle = function() { return browser.getTitle(); };
 
-  this.mainTitle = element( by.css( 'main > .wrapper > \
-                                     div > section > h1' ) );
+  this.mainTitle = element( by.css( '.qa-main-title' ) );
 
-  this.introText = element( by.css( 'main > .wrapper > \
-                                     div > section > section > \
-                                     section > div') );
+  this.introText = element( by.css( '.office_intro-text') );
 
   // TODO: Include show_subscription when page that uses this value is found.
   // this.subscription = element( by.css( '') );
 
-  this.topStoryHead = element( by.css( 'main > .wrapper > \
-                                        div > section > section:nth-child(3) > \
-                                        h2' ) );
+  this.topStoryHead = element( by.css( '.qa-top-story-head' ) );
 
-  this.topStoryDesc = element( by.css( 'main > .wrapper > \
-                                        div > section > section:nth-child(3) > \
-                                        div > div:nth-child(1) > \
-                                        p.short-desc' ) );
+  this.topStoryDesc = element( by.css( '.qa-top-story-desc' ) );
 
-  this.topStoryLinks = element( by.css( 'main > .wrapper > \
-                                         div > section > section:nth-child(3) > \
-                                         div > div > ul' ) );
+  this.topStoryLinks = element( by.css( '.qa-top-story-links' ) );
 
   // Check resources against /office-of-financial-education/
   // TODO: Include resource_title when page that uses this value is found.
   // this.resourceTitle = element( by.css( '' ) );
 
-  this.resourceImg = element( by.css( 'main > .wrapper > \
-                                       div > section > section:nth-child(4) > \
-                                       div:nth-child(2) > div > \
-                                       .media_image' ) );
+  this.resourceImg = element( by.css( '.qa-resource-img' ) );
 
-  this.resourceTitle = element( by.css( 'main > .wrapper > \
-                                         div > section > section:nth-child(4) > \
-                                         div:nth-child(2) > .media_body > \
-                                         h2' ) );
+  this.resourceTitle = element( by.css( '.qa-resource-title' ) );
 
-  this.resourceDesc = element( by.css( 'main > .wrapper > \
-                                        div > section > section:nth-child(4) > \
-                                        div:nth-child(2) > .media_body > \
-                                        .short-desc' ) );
+  this.resourceDesc = element( by.css( '.qa-resource-desc' ) );
 
-  this.resourceLink = element( by.css( 'main > .wrapper > \
-                                        div > section > section:nth-child(4) > \
-                                        div:nth-child(2) > .media_body > \
-                                        a' ) );
+  this.resourceLink = element( by.css( '.qa-resource-link' ) );
 
   // Check subpages against /plain-writing/
-  this.subpages = element( by.css( 'main > .wrapper > \
-                                    div > section > section > h1' ) );
+  this.subpages = element( by.css( '.qa-subpage h2' ) );
 
 
 }
